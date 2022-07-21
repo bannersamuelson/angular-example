c97980594103e08485595a433cc54f5d97df2b37
Banner Samuelson <bannersamuelson@gmail.com>
2022-07-21 13:15:34 -0400

    task 2 complete

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 
 import { Employee } from '../employee';
+import { Observable } from 'rxjs';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee',
@@ -9,7 +11,42 @@ import { Employee } from '../employee';
 })
 export class EmployeeComponent {
   @Input() employee: Employee;
+  directReports: Set<Employee> = new Set();
+  totalReports = 0;
 
-  constructor() {
+  constructor(private employeeService: EmployeeService, private dialog: MatDialog) { }
+
+
+  private async initReports(): Promise<[Set<Employee>, number]> {
+
+    const directReports = [];
+    for (const subordinate_id of this.employee.directReports ?? []) {
+      let child = this.employeeService.get(subordinate_id);
+      if (child instanceof Observable) {
+        child = await child.toPromise();
+      }
+      directReports.push(child);
+    }
+    let totalReports = 0;
+    const visited = new Set<number>();
+    const todo = [];
+    for (let node = this.employee; node !== undefined; node = todo.shift()) {
+      visited.add(node.id);
+      for (const child_id of (node.directReports ?? [])) {
+        if (visited.has(child_id)) {
+          continue;
+        }
+        // Value is cached in employee.service.ts so we aren't doing O(N^2) api calls.
+        let child = this.employeeService.get(child_id);
+        if (child instanceof Observable) {
+          child = await child.toPromise();
+        }
+        todo.push(child);
+      }
+      totalReports++;
+    }
+    return [new Set(directReports), totalReports - 1];
   }
+
 }
+
